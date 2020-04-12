a10f57827d17e101ce2563e353993c6aefc68ac2
Varun Kumar <varun.kumar@radisys.com>
2020-04-12 19:42:36 +0530

    Comments

--- MarkList/src/main/java/com/webtwinz/learning/marksheet/Application.java
+++ MarkList/src/main/java/com/webtwinz/learning/marksheet/Application.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-//IMPROVEMENT: Use sl4j for logging instead of System.out
+//IMPROVEMENT: Use sl4j for logging instead of System.out, read noOfStudents from command line
 public class Application {
 
   public static void main(String[] args) throws IOException {
